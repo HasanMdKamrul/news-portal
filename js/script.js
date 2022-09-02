2b9a46d4f747e6b1ed257a8e14579938f3c3c998
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-02 19:32:42 +0200

    data loader function added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-// ** Load Category data
+// ** data loader function
+
+const dataLoader = async (url)=>{
+    try {
+        const response = await fetch(url);
+        response.ok ? console.log('Successfull') : console.log('Failed');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+
+// ** Load category data
 
