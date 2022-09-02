4014e1f9a048b260ac95814c76ad62481eb7e2a8
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-02 21:07:11 +0200

    News content display upon category click done

--- js/script.js
+++ js/script.js
@@ -41,6 +41,9 @@ const displayCategories = async ()=>{
 // ** Load News by Specific category and display in the ui
 
 const displayNewsByCategory = async (id)=>{
+    // ** Where to display the news contents
+    const newsContainer = document.getElementById('news-content');
+    newsContainer.textContent = ``;
     // ** specific news loaded
     const newsLoad = await dataLoader(`https://openapi.programming-hero.com/api/news/category/${id}`);
     const {data} = newsLoad;
@@ -49,7 +52,36 @@ const displayNewsByCategory = async (id)=>{
     const highestToLowestViewNews = data.sort((a,b)=>{return (b.total_view-a.total_view)});
    
     highestToLowestViewNews.forEach(news=> {
-        const {} = news;
+        console.log(news)
+        const {author:{name,published_date,img},image_url,title,thumbnail_url,total_view,details} = news;
+        const divContent = document.createElement('div');
+        divContent.classList.add('gap-16','items-center','py-8','px-4','mx-auto','max-w-screen-xl','lg:grid','lg:grid-cols-2','lg:py-16','lg:px-6');
+        divContent.innerHTML = `
+        <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+        <h2
+          class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"
+        >
+          ${title ? title : 'N/A'}
+        </h2>
+        <p class="mb-4">
+          ${details.length > 350 ? details.slice(0,350) + '...': details}
+        </p>
+      </div>
+      <div class="grid grid-cols-2 gap-4 mt-8">
+        <img
+          class="w-full rounded-lg"
+          src="${thumbnail_url ? thumbnail_url : "../logo 2/logo.png"}"
+          alt="office content 1"
+        />
+        <img
+          class="mt-4 w-full lg:mt-10 rounded-lg"
+          src="${image_url ? image_url :"../logo 2/logo.png"}"
+          alt="office content 2"
+        />
+      </div>
+        `;
+        newsContainer.appendChild(divContent)
+
     })
 }
 
