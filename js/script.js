43b9159026d830749cadcabee135d80fb5404e2f
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-03 01:48:53 +0200

    Blog page added with the question and answer

--- js/script.js
+++ js/script.js
@@ -200,8 +200,23 @@ const sppinerToggle = isSpining =>{
   const sppinerSection = document.getElementById('sppiner');
 
   isSpining ? sppinerSection.classList.remove('hidden') : sppinerSection.classList.add('hidden');
-}
+};
 
-displayCategories()
+// ** Blog html loaded
+
+document.getElementById('blog-Button').addEventListener('click',()=>{
+  window.location.href = "http://127.0.0.1:5500/blog.html";
+});
+
+document.getElementById('news-button').addEventListener('click',()=>{
+  window.location.href = "http://127.0.0.1:5500/index.html";
+});
+
+
+// ** Display catagories
+
+displayCategories();
+
+
 
 
