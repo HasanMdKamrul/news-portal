f3c881b46393fb54a75b34c1c1f0185891ea6b1e
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-03 02:32:25 +0200

    link fixes

--- js/utilities.js
+++ js/utilities.js
@@ -1,9 +1,9 @@
 // ** Blog html loaded
 
 document.getElementById('blog-Button').addEventListener('click',()=>{
-    window.location.href = "https://news-twenty-four-seven-kamrul.netlify.app/";
+    window.location.href = "https://news-twenty-four-seven-kamrul.netlify.app/blog";
   });
   
   document.getElementById('news-button').addEventListener('click',()=>{
-    window.location.href = "https://news-twenty-four-seven-kamrul.netlify.app/blog";
+    window.location.href = "https://news-twenty-four-seven-kamrul.netlify.app/";
   });
